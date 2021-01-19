32ccb4261350d8391c1c998985e75cb3877002e9
Oskar Thoren <ot@oskarthoren.com>
2021-01-19 18:28:21 +0800

    Ask for permission to see accounts and log it

--- src/js/app.js
+++ src/js/app.js
@@ -7,24 +7,58 @@ App = {
     return App.initWeb3();
   },
 
-  initWeb3: function() {
-    if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
+  initWeb3: async function() {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      // XXX Right way to do this?
       App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
-    } else {
-      // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-      web3 = new Web3(App.web3Provider);
+      try {
+        // Request account access if needed
+        await ethereum.enable();
+        // Acccounts now exposed
+        //web3.eth.sendTransaction({/* ... */});
+        console.log("accounts 0", web3.eth.accounts[0]);
+        // XXX Seems to be treated differently in Metamask, unclear how to get a
+        // different derived key without changing account
+        console.log("accounts 1", web3.eth.accounts[1]);
+      } catch (error) {
+        // User denied account access...
+      }
     }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        console.log("3")
+      window.web3 = new Web3(web3.currentProvider);
+      App.web3Provider = web3.currentProvider;
+      // Acccounts always exposed
+      //web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
+    else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+
+    // NOTE Old code
+    // if (typeof web3 !== 'undefined') {
+    //   // If a web3 instance is already provided by Meta Mask.
+    //   App.web3Provider = web3.currentProvider;
+    //   web3 = new Web3(web3.currentProvider);
+    // } else {
+    //   // Specify default instance if no web3 instance provided
+    //   App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    //   web3 = new Web3(App.web3Provider);
+    // }
+
     return App.initContract();
+
   },
 
   initContract: function() {
     // TODO Replace me
     $.getJSON("ERC20PresetMinterPauser.json", function(erc20) {
       console.log("Hi!");
-      console.log("Hi! ", erc20)
+      //console.log("Hi! ", erc20)
       // Instantiate a new truffle contract from the artifact
       App.contracts.ERC20PresetMinterPauser = TruffleContract(erc20);
       // Connect provider to interact with contract
@@ -44,6 +78,15 @@ App = {
 
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
+      console.log("getCoinbase", account, err);
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+
+    web3.eth.getCoinbase(function(err, account) {
+      console.log("getCoinbase", account, err);
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
